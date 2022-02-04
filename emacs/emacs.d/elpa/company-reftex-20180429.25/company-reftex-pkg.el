76d9343c2c42e88c34ddd3a8c57afddc523415de
santiago <shpotes3@gmail.com>
2022-02-04 17:58:43 -0500

    remove unused dot files

--- emacs/emacs.d/elpa/company-reftex-20180429.25/company-reftex-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "company-reftex" "20180429.25" "Company backend based on RefTeX." '((emacs "25.1") (s "1.12") (company "0.8")) :commit "6a565128fdaff93be550758f2694ada7a452d06b" :keywords '("bib" "tex" "company" "latex" "reftex" "references" "labels" "citations") :url "https://github.com/TheBB/company-reftex")
